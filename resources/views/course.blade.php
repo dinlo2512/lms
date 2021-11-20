7bf7392db6453f839717489f62ac86b4df024f8b
dinlo2512 <dinhlong251200@gmail.com>
2021-11-20 13:17:29 +0700

    Update + Footer

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -26,5 +26,32 @@
                 </div>
             </div>
         </div>
+        <div id="footer">
+            <div class="footer-main">
+                <div class="container">
+                    <div class="row">
+                        <div class="logo-footer col-md-4">
+                            <img src="{{URL('front-end/images/logo2.png')}}" alt="">
+                        </div>
+                        <div class="contact-us col-md-4">
+                            <h3>contact us</h3>
+                            <p><i class="fas fa-phone-square-alt"></i> Phone: +999999999</p>
+                            <p><i class="fas fa-envelope"></i> Email:aaaa@gmail.com</p>
+                        </div>
+                        <div class="icon col-md-4">
+                            <h3>get social</h3>
+                            <img src="{{URL('front-end/images/facebook-logo.png')}}" alt="">
+                            <img src="{{URL('front-end/images/ins-logo.png')}}" alt="">
+
+                            <img src="{{URL('front-end/images/twitter-logo.png')}}" alt="">
+                            <img src="{{URL('front-end/images/google-plus.png')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h1>&copy; copyright @ 2021 by <span>DINLO</span></h1>
+
+        </div>
     </div>
 @endsection
