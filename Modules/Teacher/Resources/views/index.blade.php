bdd9b4d58f22ad1995132aa1c2a4db8cf7e68f6d
dinlo2512 <dinhlong251200@gmail.com>
2022-01-04 23:09:55 +0700

    Middleware

--- Modules/Teacher/Resources/views/index.blade.php
+++ Modules/Teacher/Resources/views/index.blade.php
@@ -6,4 +6,13 @@
     <p>
         This view is loaded from module: {!! config('teacher.name') !!}
     </p>
+    <a href="{{ route('logout') }}"
+       onclick="event.preventDefault();
+       document.getElementById('logout-form').submit();"><i
+            class="fas fa-sign-out-alt"></i>
+        <div class="name">LOG OUT</div>
+    </a>
+    <form id="logout-form" action="{{ route('teacher.logout') }}" method="POST" class="d-none">
+        @csrf
+    </form>
 @endsection
--- Modules/Teacher/Routes/web.php
+++ Modules/Teacher/Routes/web.php
@@ -13,7 +13,20 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Teacher\Http\Controllers\TeacherController;
+use Modules\Teacher\Http\Controllers\AuthController;
 
-Route::prefix('teacher')->group(function() {
-    Route::get('/', [TeacherController::class, 'index']);
+Route::prefix('teacher')->as('teacher.')->group(function() {
+    Route::middleware('auth.teacher')->group(function(){
+        Route::get('/', [TeacherController::class, 'index'])->name('home');
+        Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
+    });
+
+    Route::middleware('guest.teacher')->group(function(){
+        Route::get('/login',[AuthController::class, 'showLogin'])->name('showLogin');
+        Route::post('/login',[AuthController::class, 'login'])->name('login');
+    });
+
+
 });
+
+
